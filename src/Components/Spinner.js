a7286f8556dd6d45ab1210fd288a4db96550d469
Nahal Anjum <nahalanjun004@gmail.com>
2023-07-31 17:01:44 +0500

    Create Spinner.js

---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return <div>
+    <div className="d-flex justify-content-center my-3">
+      <div className="spinner-border text-warning" role="status"></div>
+    </div >
+  </div>;
+}
+
+export default Spinner;
